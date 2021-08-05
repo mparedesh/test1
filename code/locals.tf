a11549cf38d1950f8f6e0da7375ea0c8fdeb93da
mparedesh <mparedesh@users.noreply.github.com>
2021-08-05 03:28:55 -0500

    Initial load

---
+++ code/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  //Ecuadorian time GTM-5
+  current_time = timeadd(timestamp(), "-5h")
+}
---
+++ code/vars.tf
@@ -0,0 +1,9 @@
+variable "s3_bucket_name_prefix" {}
+variable "s3_acl" {}
+
+variable "filename_format" {}
+variable "file_count" {}
+
+variable "AWS_DEFAULT_REGION" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
